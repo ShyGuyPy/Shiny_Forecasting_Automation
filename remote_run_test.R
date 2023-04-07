1de7d8eb1bc5a8d88b063d685b763b8862eb4313
ShyGuyPy <luke.vawter@gmail.com>
2023-04-06 20:29:10 -0400

    changes after first remote test

--- remote_run_test.R
+++ remote_run_test.R
@@ -1,20 +1,35 @@
-need <- c("reticulate")
+need <- c("dplyr","reticulate")
 # find out which packages are installed
 ins <- installed.packages()[, 1]
 # check if the needed packages are installed
+print("ins=")
+print(ins)
 (Get <-
     need[which(is.na(match(need, ins)))])
+print("Get=")
+print(Get)
+
+print("Get is greater than 0")
+if (length(Get) > 0) {print("this it true")}
+
 # install the needed packages if they are not-installed
 if (length(Get) > 0) {
   install.packages(Get)
 }
+
 # load the needed packages
 eval(parse(text = paste("suppressPackageStartupMessages(library(", need, "))")))
 rm(Get, ins, need)
 
+library(dplyr)
+library(reticulate)
+
+
+
+
 #use python packaged with r project
 use_python(paste0(getwd(),"/portable_python/Python 3.9"))
 
-library(reticulate)
+
 
 source_python(paste0(getwd(),"/remote_run_test.py"))
