308357f3a12a712a7e9be6a920a548a714364d96
ShyGuyPy <luke.vawter1@gmail.com>
2023-04-10 13:45:37 -0400

    sorting dependencies

--- code/server/es_run_all.bat
+++ code/server/es_run_all.bat
@@ -1,3 +1,2 @@
 //this is a workaronud to an issue were win32com module would not import properly whe run through r reticulate module
-//python code/server/es_run_all.py
-python -v
+python code/server/es_run_all.py
--- pip_install.bat
+++ pip_install.bat
@@ -1,6 +1,7 @@
 py get-pip.py
-pip install virtualenv
 pip install --upgrade pip
+pip install virtualenv
+pip install pywin
 pip install pywin32
 pip install win32gui
 
