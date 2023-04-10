25a9cb1db99a1c25478fa84fd6a0d0c134e95ba9
ShyGuyPy <luke.vawter1@gmail.com>
2023-04-10 14:25:42 -0400

    added batch scripts for intitial setup, runtime and closing

--- pip_install.bat
+++ pip_install.bat
@@ -1,6 +1,9 @@
 py get-pip.py
 pip install --upgrade pip
 pip install virtualenv
+virtualenv -p python3 es_run_env
+es_run_env/Scripts/activate 
+pip install requirements.txt
 pip install pywin
 pip install pywin32
 pip install win32gui
---
+++ set_on_run.bat
@@ -0,0 +1,2 @@
+es_run_env/Scripts/activate
+pip install -r requirements.txt
